e38f1114868870fbedfd0cff47c498b4900d3bb0
Conor Walsh <conorwalsh0703@gmail.com>
2020-03-29 21:43:29 +0100

    added change location functionality

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 // Init Weather object
-
-
-const weather = new Weather('Castlebar', 'ie')
+const weather = new Weather('Buenos Aires', 'ar')
 const ui = new UI()
 
 // Get Weather on DOM Load
 document.addEventListener('DOMContentLoaded', getWeather)
 
+// Change location event
+document.getElementById('w-change-btn').addEventListener('click', e => {
+    const city = document.getElementById('city').value
+    const country = document.getElementById('country').value
+
+    weather.changeLocation(city, country)
+    getWeather()
+    $('#locModal').modal('hide')
+})
 
 function getWeather() {
     weather.getWeather().then(data => {
